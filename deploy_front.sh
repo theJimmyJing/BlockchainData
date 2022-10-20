1d09e1794fc63ce8b5d27a53ab676f5870542ad9
fccdev <dev@freechat.world>
2022-10-20 10:04:28 +0800

    fix

--- deploy_front.sh
+++ deploy_front.sh
@@ -4,6 +4,8 @@ rm -rf /usr/share/nginx/fcc/*
 cd /root/FreeChat-WEB || exit
 # git reset --hard
 git pull
-cp -rf build/* /usr/share/nginx/fcc/*
+npm install
+npm run build
+cp -rf build/* /usr/share/nginx/fcc/
 nginx -t
 nginx -s reload
