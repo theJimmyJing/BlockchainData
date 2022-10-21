1aa99abb6f77741c9d754555cc6a3c0110b07f9d
dongshujian <271772625@qq.com>
2022-10-21 14:21:03 +0800

    更新启动脚本和监控脚本

--- deploy_web.sh
+++ deploy_web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-ps -A | grep fcc | awk '{print $1}' | xargs kill -9 $1
+ps -A | grep fcc | grep -v monitor | awk '{print $1}' | xargs kill -9 $1
 # git pull
 go build
 nohup ./fcc >fcc.log 2>&1 &
--- monitor_fcc.sh
+++ monitor_fcc.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+#! /bin/bash
+
+while true 
+do
+	monitor=`ps -ef | grep fcc | grep -v grep| grep -v monitor | wc -l ` 
+    echo $monitor
+	if [ $monitor -eq 0 ] 
+	then
+		echo "Manipulator program is not running, restart Manipulator"
+		./deploy_web.sh &
+	else
+		echo "Manipulator program is running"
+	fi
+	sleep 5
+done
