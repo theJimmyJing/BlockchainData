92c35a55928aac37fa3240e1571a298a65c890c5
dongshujian <271772625@qq.com>
2022-10-29 20:56:46 +0800

    修改包活脚本，使用crontab执行定时检查

--- monitor_fcc.sh
+++ monitor_fcc.sh
@@ -2,8 +2,13 @@
 
 #! /bin/bash
 
-while true 
-do
+# 使用crontab定时执行
+# crontab -e 然后输入
+# * * * * *  /..路径/Tokenlist/monitor_fcc.sh 2&>1 >> /路径../check-alive.log  #每分钟检查一次
+# */5 * * * *  /..路径/Tokenlist/monitor_fcc.sh 2&>1 >> /路径../check-alive.log  #每5分钟检查一次
+
+# while true 
+# do
 	monitor=`ps -ef | grep fcc | grep -v grep| grep -v monitor | wc -l ` 
 	if [ $monitor -eq 0 ] 
 	then
@@ -11,6 +16,6 @@ do
 		./deploy_web.sh &
 	# else
 	# 	echo "fcc program is running"
-	fi
-	sleep 10
-done
+	# fi
+	# sleep 10
+# done
