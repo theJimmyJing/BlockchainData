50935fe792a546eb542ecd5346ad378866ac1a0b
theJimmyJing <118824781+theJimmyJing@users.noreply.github.com>
2022-11-24 13:26:29 +0800

    Update deploy_web.sh
    将config.yaml文件私有化
    将blockchaindata fcc进程以systemd的方式启动
    变更了部署流程。

--- deploy_web.sh
+++ deploy_web.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-set -x
-ps -A | grep fcc | grep -v monitor | awk '{print $1}' | xargs kill -9 $1
-git pull
-go build
-nohup ./fcc >fcc.log 2>&1 &
+#set -x
+#ps -A | grep fcc | grep -v monitor | awk '{print $1}' | xargs kill -9 $1
+#git pull
+#go build
+#nohup ./fcc >fcc.log 2>&1 &
+
+git pull && cd ../Private_Config
+git pull && cp -rv ./blockchain-config/config.yaml ../BlockchainData/config/ && cd ../BlockchainData
+go build && sudo systemctl restart blockchaindata
